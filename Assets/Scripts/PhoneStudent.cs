7591b0a555bb8aaf1cfc11557007a66553b24912
Matko <brieska.martin@gmail.com>
2018-11-07 23:53:50 +0100

    Troška dačo som srapvil už som zabudol čo

---
+++ Assets/Scripts/PhoneStudent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneStudent : Student {
+
+
+    // Use this for initialization
+    private new void Start () {
+        base.Start();
+        animationName = "UsingPhone";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
