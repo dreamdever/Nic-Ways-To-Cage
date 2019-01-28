7f717fcd1a8e844637ed5b2ddcb02c860dadac16
Andrej Karkuš <andrej.karkus@elitesolutions.sk>
2019-01-28 20:51:51 +0100

    add volume controller

---
+++ Assets/VolumeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour {
+    public Slider slider = null;
+
+    public void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        slider = gameObject.GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        Debug.Log(slider.value);
+        AudioListener.volume = slider.value;
+    }
+}
